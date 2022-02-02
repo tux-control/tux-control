a272f9000d4241c1fd64685e3fc72a6545c5c18e
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-02 12:16:51 +0100

    fix(Typing): Use union instead of |

--- tux_control/plugin/validators/NumberValidator.py
+++ tux_control/plugin/validators/NumberValidator.py
@@ -1,12 +1,12 @@
 import re
-from typing import Type
+from typing import Type, Union
 from tux_control.plugin.validators.PatternValidator import PatternValidator
 
 
 class NumberValidator(PatternValidator):
     message = None
 
-    def __init__(self, number_type: Type[int | float] = int, message: str = 'Please enter valid number.'):
+    def __init__(self, number_type: Type[Union[int, float]] = int, message: str = 'Please enter valid number.'):
         self.pattern = r'^(?:-|)\d+$' if number_type is int else r'^(?:-|)(?:\d+.\d+|\d+)$'
         self.message = message
 
