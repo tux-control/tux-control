e33e68d49911aff377ee7ca72cf191dc2f83ffa9
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-02 03:50:01 +0100

    New version 0.0.5

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ extra_files = [
 
 setup(
     name='tux-control',
-    version='0.0.4',
+    version='0.0.5',
     description='Tux Control',
     long_description=open('README.md').read(),
     author='Adam Schubert',
