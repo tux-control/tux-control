0c84a341e04d63ed9f528ce28d0c189b1f660b11
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-07 10:00:14 +0100

    feat(plugin.Control): Add mimeType filter to file input

--- tux_control/plugin/controls/File.py
+++ tux_control/plugin/controls/File.py
@@ -1,3 +1,4 @@
+from typing import List
 from tux_control.plugin.controls.IControl import IControl
 from tux_control.tools.IDictify import IDictify
 
@@ -6,8 +7,9 @@ class File(IControl, IDictify):
 
     html_type = 'file'
 
-    def __init__(self, placeholder: str = None):
+    def __init__(self, placeholder: str = None, match_mimetypes: List[str] = None):
         self._placeholder = placeholder
+        self.match_mimetypes = match_mimetypes
 
     @property
     def placeholder(self) -> str:
@@ -16,7 +18,8 @@ class File(IControl, IDictify):
     def to_dict(self) -> dict:
         return {
             'placeholder': self.placeholder,
-            'html_type': self.html_type
+            'html_type': self.html_type,
+            'match_mimetypes': self.match_mimetypes,
         }
 
     @staticmethod
