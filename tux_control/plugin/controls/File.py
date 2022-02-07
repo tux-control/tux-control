86ada1800cea7ad48e056788e4e7ce49bf7950ed
Adam Schubert <adam.schubert@sg1-game.net>
2022-02-07 10:03:27 +0100

    feat(control.File): Added picker type

--- tux_control/plugin/controls/File.py
+++ tux_control/plugin/controls/File.py
@@ -1,15 +1,23 @@
+import enum
 from typing import List
 from tux_control.plugin.controls.IControl import IControl
 from tux_control.tools.IDictify import IDictify
+
+
+class FilePickerType(enum.Enum):
+    ALL = 'all'
+    FILE = 'file'
+    DIRECTORY = 'directory'
 
 
 class File(IControl, IDictify):
 
     html_type = 'file'
 
-    def __init__(self, placeholder: str = None, match_mimetypes: List[str] = None):
+    def __init__(self, placeholder: str = None, picker_type: FilePickerType = FilePickerType.ALL,  match_mimetypes: List[str] = None):
         self._placeholder = placeholder
         self.match_mimetypes = match_mimetypes
+        self.picker_type = picker_type
 
     @property
     def placeholder(self) -> str:
@@ -20,6 +28,7 @@ class File(IControl, IDictify):
             'placeholder': self.placeholder,
             'html_type': self.html_type,
             'match_mimetypes': self.match_mimetypes,
+            'picker_type': self.picker_type.value,
         }
 
     @staticmethod
